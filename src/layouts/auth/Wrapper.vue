e805c15bc33c7b9e4eb3d2b50cb19b11677f13c0
PeterKitonga <pkitonga.pk@gmail.com>
2019-04-14 22:00:57 +0300

    Added a guest and auth wrapper

---
+++ src/layouts/auth/Wrapper.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-container class="custom-container">
+        <el-aside width="200px">
+            <router-view name="sidebar"></router-view>
+        </el-aside>
+        <el-container>
+            <el-header class="no-padding">
+                <router-view name="header"></router-view>
+            </el-header>
+            <router-view/>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "auth-wrapper"
+    }
+</script>
+
+<style scoped>
+    .custom-container {
+        height: 100%;
+        width: 100%;
+    }
+    .no-padding{
+        padding: 0
+    }
+</style>
---
+++ src/layouts/guest/Wrapper.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-container class="custom-container">
+        <el-header class="no-padding">
+            <router-view name="header"></router-view>
+        </el-header>
+        <router-view></router-view>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "guest-wrapper"
+    }
+</script>
+
+<style scoped>
+    .custom-container {
+        height: 100%;
+        width: 100%;
+    }
+    .no-padding{
+        padding: 0
+    }
+</style>
