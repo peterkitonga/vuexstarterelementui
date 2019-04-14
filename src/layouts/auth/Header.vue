45aa47e332e162832e42c00a6b2ec1f94a2be972
PeterKitonga <pkitonga.pk@gmail.com>
2019-04-14 22:01:18 +0300

    Added a guest and auth header components

---
+++ src/layouts/auth/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-menu class="el-menu-custom" mode="horizontal" :default-active="this.$route.name" :router="true">
+        <el-menu-item index="logo" route="/">VuexStarter</el-menu-item>
+        <el-menu-item index="logout" route="#" style="float: right">
+            <div class="image">
+                <el-dropdown @command="handleCommands" trigger="click">
+                    <span>
+                        <img src="/images/user-avatar.png" alt="User Image" class="img-circle" style="height: 35px;width: 35px;border-radius: 50%">
+                        <i class="el-icon-arrow-down el-icon--right"></i>
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item :command="{method: 'logout', params: {}}">Logout</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+            </div>
+        </el-menu-item>
+    </el-menu>
+</template>
+
+<script>
+    export default {
+        name: "auth-header",
+        methods: {
+            handleCommands: function (command) {
+                this[command.method](command.params);
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/guest/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-menu class="el-menu-custom" mode="horizontal" :default-active="this.$route.name" :router="true">
+        <el-menu-item index="logo" route="/">VuexStarter</el-menu-item>
+        <el-menu-item index="register" route="/register" style="float: right">Register</el-menu-item>
+        <el-menu-item index="login" route="/login" style="float: right">Login</el-menu-item>
+    </el-menu>
+</template>
+
+<script>
+    export default {
+        name: "guest-header"
+    }
+</script>
+
+<style scoped>
+
+</style>
