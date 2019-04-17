353e1cca8054507118a2f902a9c27851676994c6
PeterKitonga <pkitonga.pk@gmail.com>
2019-04-17 23:12:55 +0300

    Added a component for the administrator dashboard

---
+++ src/views/auth/dashboards/Administrator.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-main>
+        <el-row>
+            <el-col></el-col>
+        </el-row>
+    </el-main>
+</template>
+
+<script>
+    export default {
+        name: "administrator-dashboard"
+    }
+</script>
+
+<style scoped>
+
+</style>
