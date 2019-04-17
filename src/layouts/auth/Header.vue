a4f29fe766cd9a74c7b0aaec06b80712647a1804
PeterKitonga <pkitonga.pk@gmail.com>
2019-04-17 22:50:26 +0300

    Added logout functionality to the auth header component

--- src/layouts/auth/Header.vue
+++ src/layouts/auth/Header.vue
@@ -2,28 +2,45 @@
     <el-menu class="el-menu-custom" mode="horizontal" :default-active="this.$route.name" :router="true">
         <el-menu-item index="logo" route="/">VuexStarter</el-menu-item>
         <el-menu-item index="logout" route="#" style="float: right">
-            <div class="image">
-                <el-dropdown @command="handleCommands" trigger="click">
+            <el-dropdown @command="handleCommands" trigger="click">
                     <span>
                         <img src="/images/user-avatar.png" alt="User Image" class="img-circle" style="height: 35px;width: 35px;border-radius: 50%">
                         <i class="el-icon-arrow-down el-icon--right"></i>
                     </span>
-                    <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item :command="{method: 'logout', params: {}}">Logout</el-dropdown-item>
-                    </el-dropdown-menu>
-                </el-dropdown>
-            </div>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item :command="{method: 'logout', params: {}}">Logout</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
         </el-menu-item>
     </el-menu>
 </template>
 
 <script>
+    import {INIT_LOGOUT_AUTH_USER} from "../../store/types";
+
     export default {
         name: "auth-header",
         methods: {
             handleCommands: function (command) {
                 this[command.method](command.params);
             },
+            logout: function () {
+                return this.$store.dispatch(INIT_LOGOUT_AUTH_USER).then(response => {
+                    this.$message({
+                        type: response.status,
+                        showClose: true,
+                        duration: 10000,
+                        message: response.message,
+                    });
+                }).catch(error => {
+                    this.$message({
+                        type: 'error',
+                        showClose: true,
+                        duration: 10000,
+                        message: error.data.message,
+                    });
+                })
+            }
         }
     }
 </script>
