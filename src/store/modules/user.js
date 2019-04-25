2ef36ca7b669107f6717ac19e5fb202a4dad190d
PeterKitonga <pkitonga.pk@gmail.com>
2019-04-25 22:42:43 +0300

    Added actions and mutation types for pulling users data

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,22 +1,63 @@
 import {
-
+    GET_ALL_USERS,
+    GET_AUTH_TOKEN,
+    INIT_FETCH_ALL_USERS,
+    INIT_FETCH_PAGINATED_USERS, SET_ALL_USERS
 } from '../types'
 import HTTP from '../../http'
 
 const state = {
-
+    all_users: []
 };
 
 const getters = {
-
+    [GET_ALL_USERS]: state => {
+        return state.all_users;
+    }
 };
 
 const mutations = {
-
+    [SET_ALL_USERS]: (state, payload) => {
+        state.all_users = payload.data;
+    },
 };
 
 const actions = {
-
+    [INIT_FETCH_ALL_USERS]: ({getters, commit}, payload) => {
+        return new Promise((resolve, reject) => {
+            HTTP.get('users', {
+                headers: {
+                    Authorization: getters[GET_AUTH_TOKEN]
+                },
+                params: {
+                    page: payload.page,
+                    limit: payload.limit
+                }
+            }).then(response => {
+                commit(SET_ALL_USERS, response.data);
+                resolve(response.data);
+            }).catch(error => {
+                reject(error.response);
+            });
+        });
+    },
+    [INIT_FETCH_PAGINATED_USERS]: ({getters, commit}, payload) => {
+        return new Promise((resolve, reject) => {
+            HTTP.get('users', {
+                headers: {
+                    Authorization: getters[GET_AUTH_TOKEN]
+                },
+                params: {
+                    page: payload.page,
+                    limit: payload.limit
+                }
+            }).then(response => {
+                resolve(response.data);
+            }).catch(error => {
+                reject(error.response);
+            });
+        });
+    }
 };
 
 export default {
--- src/store/types.js
+++ src/store/types.js
@@ -5,12 +5,18 @@ export const GET_AUTH_REFRESH_TOKEN = 'GET_AUTH_REFRESH_TOKEN';
 export const GET_AUTH_OBJECT = 'GET_AUTH_OBJECT';
 export const GET_AUTH_LOGIN_STATUS = 'GET_AUTH_LOGIN_STATUS';
 
+// user
+export const GET_ALL_USERS = 'user/GET_ALL_USERS';
+
 /*================================= Mutations ===============================================================================*/
 // auth
 export const SET_AUTH_OBJECT = 'SET_AUTH_OBJECT';
 export const SET_AUTH_LOGIN_STATUS = 'SET_AUTH_LOGIN_STATUS';
 export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
 export const SET_AUTH_REFRESH_TOKEN = 'SET_AUTH_REFRESH_TOKEN';
+
+// user
+export const SET_ALL_USERS = 'user/SET_ALL_USERS';
 
 /*================================= Actions ===============================================================================*/
 // auth
@@ -23,3 +29,11 @@ export const INIT_FETCH_AUTH_USER = 'INIT_FETCH_AUTH_USER';
 export const INIT_UPDATE_AUTH_USER_PROFILE = 'INIT_UPDATE_AUTH_USER_PROFILE';
 export const INIT_UPDATE_AUTH_USER_PASSWORD = 'INIT_UPDATE_AUTH_USER_PASSWORD';
 export const INIT_LOGOUT_AUTH_USER = 'INIT_LOGOUT_AUTH_USER';
+
+// user
+export const INIT_FETCH_ALL_USERS = 'user/INIT_FETCH_ALL_USERS';
+export const INIT_FETCH_PAGINATED_USERS = 'user/INIT_FETCH_PAGINATED_USERS';
+export const INIT_STORE_USER = 'user/INIT_STORE_USER';
+export const INIT_UPDATE_USER_ROLE = 'user/INIT_UPDATE_USER_ROLE';
+export const INIT_UPDATE_USER_ACTIVE_STATUS = 'user/INIT_UPDATE_USER_ACTIVE_STATUS';
+export const INIT_DELETE_USER = 'user/INIT_DELETE_USER';
